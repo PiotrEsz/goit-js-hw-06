86f9c48c48ade330c53d83b642d70c7b010b7022
Piotr Szatyński <szatynskip@gmail.com>
2022-12-15 09:49:53 +0100

    Update task-02.js

--- src/src/js/task-02.js
+++ src/src/js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const findUl = document.querySelector("#ingredients");
+
+const secondTask = ingredients.forEach((ingredient) => {
+  let listOfIngredient = document.createElement("li");
+  listOfIngredient.textContent = ingredient;
+  listOfIngredient.classList.add("item");
+  findUl.append(listOfIngredient);
+});
