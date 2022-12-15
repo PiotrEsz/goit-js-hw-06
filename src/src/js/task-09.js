43ae95f0cf561f9359159abcae00902cb0d98c2d
Piotr Szatyński <szatynskip@gmail.com>
2022-12-15 09:55:48 +0100

    Update task-09.js

--- src/src/js/task-09.js
+++ src/src/js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonChangeColor = document.querySelector(".change-color");
+const spanColor = document.querySelector(".color");
+
+buttonChangeColor.addEventListener("click", () => {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  spanColor.textContent = color;
+});
