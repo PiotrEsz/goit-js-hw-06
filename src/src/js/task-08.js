28feb01ac765a3a6b195609ed74c31226dee498f
Piotr Szatyński <szatynskip@gmail.com>
2022-12-15 09:55:18 +0100

    Update task-08.js

--- src/src/js/task-08.js
+++ src/src/js/task-08.js
@@ -1,1 +1,17 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  if (!email || !password) {
+    alert("Please fill in all fields");
+    return;
+  }
+
+  const loginData = { email, password };
+  console.log(loginData);
+  form.reset();
+});
